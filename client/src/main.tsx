5eb83722336a00f066815dae380a575a22bd8e5e
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2024-06-06 10:19:17 +0200

    Starting front

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App';
+
+import Home from '@/routes/Home/Home';
+import Signin from '@/routes/Signin/Signin';
+import Signup from '@/routes/Signup/Signup';
+
+import './index.css';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/signin',
+				element: <Signin />
+			},
+			{
+				path: '/signup',
+				element: <Signup />
+			},
+		],
+	},
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);
